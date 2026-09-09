398f215175978281ba74862fc2527d0f10fdfa85
unknown <sankarnandi1010@gmail.com>
2026-09-10 01:28:40 +0530

    fix

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Admin\AnalyticsController;
 use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 })->middleware('auth');
 
 
